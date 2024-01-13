d3c92a0c3960e166bd8f10bb79133c13ef4b4fd4
Valkna <59378772+Valkna@users.noreply.github.com>
2024-01-13 19:31:12 -0300

    curso Batistti JS

---
+++ 5_string avancado/script.js
@@ -0,0 +1,6 @@
+console.log("Primeira linha \n Segunda linha");
+//\n é um caractere de quebra de linha, e o \ é us
+console.log("O meu nome é 'Vanessa'.");
+console.log(`O meu nome é "Vanessa".`);
+console.log(`O resultado de 5x3 é ${5*3}.`);
+console.log("O " + "meu " + "nome " + "é " + "Vanessa.");
---
+++ 7_exercicios_dados/script..js
@@ -0,0 +1,3 @@
+console.log("Vanessa");
+console.log('Vanessa');
+console.log(`Vanessa`);
